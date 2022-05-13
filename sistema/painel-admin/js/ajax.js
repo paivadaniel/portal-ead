962c2c16336b6852ab879e513b4e7763e920aaa6
Daniel Paiva (W10/Quadra) <danielantunespaiva@gmail.com>
2022-05-12 22:29:40 -0300

    mod02 aula28

---
+++ sistema/painel-admin/js/ajax.js
@@ -0,0 +1,6 @@
+function inserir() {
+    $('#mensagem').text('');
+    $('#tituloModal').text('Inserir Registro');
+    $('#modalForm').modal('show'); //abrir a modal por script, a outra forma é abrir via data-target
+    //limparCampos();
+}   
--- sistema/painel-admin/paginas/alunos.php
+++ sistema/painel-admin/paginas/alunos.php
@@ -3,8 +3,10 @@
 require_once('../conexao.php');
 require_once('verificar.php'); //aqui é dado @session_start();
 
+$pag= 'alunos';
+
 //apenas administradores podem acessar, professores não
-if(@$_SESSION['nivel'] != 'Administrador') { //coloca @ para se caso não existir alguma das variáveis de sessão, não exibir o warning
+if (@$_SESSION['nivel'] != 'Administrador') { //coloca @ para se caso não existir alguma das variáveis de sessão, não exibir o warning
     echo "<script> window.location='index.php'</script>";
     exit(); //se o usuário malicioso desativar o script, o exit() impedirá que o restante do código seja mostrado para o usuário
 }
@@ -16,14 +18,14 @@ if(@$_SESSION['nivel'] != 'Administrador') { //coloca @ para se caso não existi
 
 <!-- div id="listar", quando for chamo listar-aluno.php, o resultado dele ele passará para id="listar" -->
 <div class="bs-example widget-shadow" style="padding:15px" id="listar">
-   
+
 </div>
 
 
 
 <!-- id="modalForm", h4 com id="tituloModal" e form com id="form" serão padrão -->
 <div class="modal fade" id="modalForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title" id="tituloModal">Editar Dados</h4>
@@ -38,13 +40,13 @@ if(@$_SESSION['nivel'] != 'Administrador') { //coloca @ para se caso não existi
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Nome</label>
-                                <input type="text" class="form-control" name="nome_usu" value="<?php echo $nome_usuario ?>" required>
+                                <input type="text" class="form-control" name="nome" id="nome" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>CPF</label>
-                                <input type="text" class="form-control" id="cpf_usu" name="cpf_usu" value="<?php echo $cpf_usuario ?>" required>
+                                <input type="text" class="form-control" name="cpf" id="cpf" required>
                             </div>
                         </div>
 
@@ -55,13 +57,44 @@ if(@$_SESSION['nivel'] != 'Administrador') { //coloca @ para se caso não existi
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Email</label>
-                                <input type="email" class="form-control" name="email_usu" value="<?php echo $email_usuario ?>" required>
+                                <input type="email" class="form-control" id="email" name="email" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label>Senha</label>
-                                <input type="password" class="form-control" name="senha_usu" value="<?php echo $senha_usuario ?>" required>
+                                <label>Telefone</label>
+                                <input type="text" class="form-control" id="telefone" name="telefone">
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Endereço</label>
+                                <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Rua X Número 20 Bairro Y">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Cidade</label>
+                                <input type="text" class="form-control" id="cidade" name="cidade">
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Estado</label>
+                                <input type="text" class="form-control" id="estado" name="estado">
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>País</label>
+                                <input type="text" class="form-control" id="pais" name="pais">
                             </div>
                         </div>
 
@@ -72,27 +105,22 @@ if(@$_SESSION['nivel'] != 'Administrador') { //coloca @ para se caso não existi
                         <div class="col-md-8">
                             <div class="form-group">
                                 <label>Foto</label>
-                                <input type="file" name="foto" onChange="carregarImgPerfil();" id="foto-usu">
+                                <input type="file" id="foto" name="foto" onChange="carregarImg();">
                             </div>
                         </div>
                         <div class="col-md-4">
                             <div id="divImg">
-                                <img src="img/perfil/<?php echo $foto_usuario ?>" width="100px" id="target-usu">
+                                <img src="img/perfil/sem-perfil.jpg" width="100px" id="target-usu">
                             </div>
                         </div>
 
                     </div>
 
-                    <input type="hidden" name="id_usu" value="<?php echo $id_usuario ?>">
-                    <input type="hidden" name="foto_usu" value="<?php echo $foto_usuario ?>">
-
-                    <input type="hidden" name="antigoEmail" value="<?php echo $email_usuario; ?>">
-                    <input type="hidden" name="antigoCpf" value="<?php echo $cpf_usuario; ?>">
-
-
                     <small>
-                        <div id="mensagem-usu" align="center" class="mt-3"></div>
+                        <div id="mensagem" align="center" class="mt-3"></div>
                     </small>
+
+                    <input type="hidden" name="id" id="id">
 
                 </div>
 
@@ -106,3 +134,6 @@ if(@$_SESSION['nivel'] != 'Administrador') { //coloca @ para se caso não existi
 </div>
 
 
+<script type="text/javascript"> var pag = "<?=$pag?>" </script>
+
+<script src="js/ajax.js"></script>
